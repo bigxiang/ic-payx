c6b8a4b68fd80c50671060cd8e94d780ebdca070
Terry.Tu <tuminfei1981@gmail.com>
2025-10-30 16:30:15 +0800

    erc20_permit

---
+++ erc20_permit/contracts/ERC20Permit.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenAiPay is ERC20, ERC20Permit, Ownable {
+    constructor(
+        address initialOwner
+    )
+        ERC20("USDT_AiPay", "USDT")
+        ERC20Permit("USDT_AiPay")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
